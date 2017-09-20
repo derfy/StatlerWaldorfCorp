5bd4bcaedac95f8186ef7fb62532b07dc618f484
ferdinando aprovitolo <derfy74@gmail.com>
2017-09-20 13:47:21 +0200

    Initial commit

---
+++ src/StatlerWaldorfCorp.TeamService/Persistence/ITeamRepository.cs
@@ -0,0 +1,16 @@
+﻿using StatlerWaldorfCorp.TeamService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatlerWaldorfCorp.TeamService.Persistence
+{
+    public interface ITeamRepository
+    {
+        IEnumerable<Team> List();
+        Team Get(Guid id);
+        Team Add(Team team);
+        Team Update(Team team);
+        Team Delete(Guid id);
+    }
+}
